476d534dfc3adb6b25c5dfe799231d69f2fd4071
FranklinShera <fshera96@gmail.com>
2021-03-31 01:56:19 +0300

    BUILD

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,7 @@ const Header = () => {
 
     const [dropNav, setdropNav] = useState(false)
 
-    function toggleNav(){
-
-
-        (dropNav) ? setdropNav(false) : setdropNav(true);
-
-        
-    }
+  
 
 
     return (
@@ -25,20 +19,23 @@ const Header = () => {
             </div>
         </div>
 
-        <div className="toggler" onClick={(e) => toggleNav()}>
-            <span className="menu-line-one"></span>
-            <span className="menu-line-two"></span>
-            <span className="menu-line-three"></span>
+        <div className={(dropNav) ? 'toggler justify-center' : 'toggler justify-between'} onClick={(e) => setdropNav(!dropNav)}>
+                        
+            <span className={(dropNav) ? 'menu-line frwdslash' : 'menu-line'}></span>
+            <span className={(dropNav) ? 'menu-line backslash' : 'menu-line'}></span>
+            {(!dropNav) && <span className="menu-line"></span>}
+
+            
         </div>
 
         <div className=" nav-right">
-            <li className=" navlink hover:border-b-2">
+            <li className=" navlink hover:border-b-2" onClick={(e) => setdropNav(false)}>
                 <a href="#navigation">Home</a>
             </li>
-            <li className="navlink hover:border-b-2">
+            <li className="navlink hover:border-b-2" onClick={(e) => setdropNav(false)}>
                 <a href="#services">Services</a>
             </li>
-            <li className="navlink hover:border-b-2">
+            <li className="navlink hover:border-b-2" onClick={(e) => setdropNav(false)}>
                 <a href="#contact">Contact</a>
             </li>
            
@@ -47,7 +44,7 @@ const Header = () => {
     </nav>
 
    {(dropNav) && (
-         <div id="mobmenu" className="flex items-center justify-center mobile-nav sm:hidden">
+         <div id="mobmenu" className="flex items-center justify-center mobile-nav sm:hidden animate__animated animate__slideInDown">
          <div className="flex flex-col items-center justify-center mid">
              <li className="my-8 navlink hover:border-b-2">
                  <a href="#navigation">Home</a>
--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -5,23 +5,44 @@ import  Logo from '.././images/as21logo.png';
 const Overlay = () => {
 
     const [loading, setLoading] = useState(true)
+    const [timerOff, setTimerOff] = useState(false)
+    const [hasReady, setHasReady] = useState(false)
 
 
-    // useEffect(() => {
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (hasReady) {
+                setLoading(false)
+                setTimerOff(true)
+            }
+        },3000)
+
+
+    },[])
+
+    
+    useEffect(() => {
         
-    //      setTimeout(() => {
-    //          setLoading(false)
-    //      },3000)
+       
+          document.onreadystatechange = () => {
+            if (document.readyState === 'complete' && timerOff) {
+                setLoading(false)
+                setHasReady(true)
+            }
+          };
+          
+
+    }, [])
 
 
-    // }, [])
-
+  
     return (
         <>
             { (loading) && (                
-                <div className="overlay">
+                <div className={(loading) ? 'overlay' : 'hidden'}>
                     <div className="inn-overlay">
-                        <img src={Logo} className="h-20" alt="academiasteph21 logo"/>
+                        <img src={Logo} className="h-20" alt="Academiasteph21 Loading logo"/>
                         <span className="mt-2 text-3xl" data-text="AcademiaSteph21">AcademiaSteph21</span>
                     </div>
                 </div>
